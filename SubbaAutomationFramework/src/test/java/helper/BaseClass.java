104575789498d0e82af60df870831eb69a8c95b0
subbarao <subbaraosoftwaretesting@gmail.com>
2019-11-07 17:25:35 +0800

    base one

--- SubbaAutomationFramework/src/test/java/helper/BaseClass.java
+++ SubbaAutomationFramework/src/test/java/helper/BaseClass.java
@@ -9,16 +9,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Configuration;
+
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-import dataProviderFactory.DataProviderFactory;
+
 
 public class BaseClass {
 	
